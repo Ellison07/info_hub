4fb2f2300d391037b70b0e8d4ca730a496dc3a26
Ellison07 <118790615+Ellison07@users.noreply.github.com>
2024-06-25 17:03:10 +0530

    first comit
    kqjwlkqjw

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Simple News Page</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
